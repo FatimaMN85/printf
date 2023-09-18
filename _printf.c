4041eaa9d4fe5de97d4748ea73d64435acb00ef7
Rahma <helmyrahma4@gmail.com>
2023-09-18 12:35:50 -0700

    update

--- _printf.c
+++ _printf.c
@@ -31,7 +31,7 @@ int _printf(const char *format, ...)
 					handleString(va_arg(args, char *), &count);
 					break;
 				case 'd' || 'i':
-					handleInteger(va_arg(args, int), &count);
+					printInteger(va_arg(args, int), &count);
 					break;
 				case '%':
 					write(1, "%", 1);
